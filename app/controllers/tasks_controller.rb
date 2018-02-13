c2ed9782c74bdc593666389688341d4571437705
Sebastien Balas <balas.sebastien@gmail.com>
2018-02-13 15:27:40 +0100

    user can edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    if @task.completed
+      @task_completed = "This task is completed"
+    else
+      @task_completed = "This task is not completed"
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180213105717) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed", default: false
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
