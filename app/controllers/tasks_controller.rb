62d16df98d4b322a7868b29b2e4eef6cde2f9141
Sebastien Balas <balas.sebastien@gmail.com>
2018-02-13 16:09:14 +0100

    task manager done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
     if @task.completed
       @task_completed = "This task is completed"
     else
@@ -17,24 +18,33 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def create
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
     # Will raise ActiveModel::ForbiddenAttributesError
   end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(params[:task])
+    @task.update(task_params)
+    redirect_to tasks_path
     # Will raise ActiveModel::ForbiddenAttributesError
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
